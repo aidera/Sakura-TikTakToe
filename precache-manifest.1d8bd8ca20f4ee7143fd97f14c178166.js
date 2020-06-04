b3dc1b0e3c243a3302f762cafa0a62bfe0c63593
Evgenia <hope.aidera@gmail.com>
2020-06-04 22:08:03 +0300

    Updates

---
+++ precache-manifest.1d8bd8ca20f4ee7143fd97f14c178166.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1af8b1633805469aacf2ee49e78478aa",
+    "url": "/Sakura-TikTakToe/index.html"
+  },
+  {
+    "revision": "f6c5ce52a32fd604ba88",
+    "url": "/Sakura-TikTakToe/static/css/main.4ecaef90.chunk.css"
+  },
+  {
+    "revision": "edfdbaf2d9d49860a739",
+    "url": "/Sakura-TikTakToe/static/js/2.24dbdc15.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Sakura-TikTakToe/static/js/2.24dbdc15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6c5ce52a32fd604ba88",
+    "url": "/Sakura-TikTakToe/static/js/main.102b17d5.chunk.js"
+  },
+  {
+    "revision": "b4078f85acdddc91b81a",
+    "url": "/Sakura-TikTakToe/static/js/runtime-main.df8982af.js"
+  },
+  {
+    "revision": "61e0921b640181183fe2c16d0cf76ce7",
+    "url": "/Sakura-TikTakToe/static/media/mat.61e0921b.png"
+  },
+  {
+    "revision": "f4a31b4b91008304e5d28816971309a7",
+    "url": "/Sakura-TikTakToe/static/media/sakura.f4a31b4b.svg"
+  },
+  {
+    "revision": "d7240e71fc17135cd81a1551882c194b",
+    "url": "/Sakura-TikTakToe/static/media/table.d7240e71.png"
+  }
+]);
